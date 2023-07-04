1bb2724ef5a539df1256acb7c5a8b4696e62a189
Amalkumar G <102892250+AmalkumarG@users.noreply.github.com>
2023-07-04 11:00:47 +0530

    Update Assignment2.py

--- Assignment2.py
+++ Assignment2.py
@@ -1,4 +1,4 @@
-# Write a Python program to get a list, sorted in increasing order by
+#1) Write a Python program to get a list, sorted in increasing order by
 # the last element in each tuple from a given list of non-empty tuples
 
 x=[(4, 4),(2, 1),(2, 5),(2, 3),(1, 2)]
@@ -10,7 +10,7 @@ for i in range(len(x)): #sort the list with respect to last value of tuples
             x[j+1]=temp
 print(x)# output --> [(2, 1), (1, 2), (2, 3), (4, 4), (2, 5)]
 
-# Write a Python program to print a dictionary whose keys should be the 
+#2) Write a Python program to print a dictionary whose keys should be the 
 # alphabet from a-z and the value should be corresponding ASCII values
 
 ascii_val={}
