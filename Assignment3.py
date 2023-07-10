6f61157db6079936a7e266a8c589f09361d56683
Amalkumar G <102892250+AmalkumarG@users.noreply.github.com>
2023-07-10 21:45:32 +0530

    Assignment3.py

--- Assignment3.py
+++ Assignment3.py
@@ -1,6 +1,6 @@
 #1) Write a Python function to sum all the numbers in a list.
 
-def list_sum(l):#calculate sum of elements in list
+def list_sum(l):#calculate sum of elements in list [8,2,3,0,7]
     sum=0
     for i in l:
         sum+=i
@@ -8,18 +8,18 @@ def list_sum(l):#calculate sum of elements in list
 list=[]
 for i in range(int(input("Enter length of list "))):
     list.append(int(input(f"Enter element{i+1} ")))#add element to list
-print(list_sum(list))
+print(list_sum(list)) #Output --> 20
 
 #2) Write a Python program to reverse a string.
 
-def rev_string(rev):
+def rev_string(rev):# 1234abcd
     return rev[::-1]#reverse string
 string=input()
-print(rev_string(string))
+print(rev_string(string)) #Output -->dcba4321
 
 #3) Write a Python function that accepts a string and calculate the number of upper case letters and lower case letters.
 
-def count_cases(string):
+def count_cases(string): #The quick Brow Fox
     upper_case,lower_case=0,0
     for i in string:
         if i.isupper()==True:#if character is uppercase
@@ -30,5 +30,7 @@ def count_cases(string):
 string=input()
 upper_case,lower_case=count_cases(string)
 print(f"No. of upper case letters: {upper_case}\nNo of lower case letters: {lower_case}")
+#Output--> No. of upper case letters: 3
+         # No of lower case letters: 12
 
 
